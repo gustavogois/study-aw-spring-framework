f0fd9a9af6ec542e0a7afd728fb48e012925937e
gustavogois <gustavo.gois@gmail.com>
2018-03-10 00:44:24 +0100

    1) Controler devolvendo o nome da View 2) Configurando a ViewResolver com Thymeleaf
    Já é possível fazer o teste com a URL: http://localhost:8080/brewer/cervejas/novo/
    
    Lembrando que:
    1) A requisição chega ao DispatcherServlet
    2) DispatcherServlet acha o controler através do HandlerMapping
    3) DispatcherServlet aciona o Controler
    4) Controller retorna dados e o nome da ViewResolver
    5) DispatcherServlet utiliza ViewResolver para acessar a View
    6) View é retornada

--- src/main/java/study/brewer/config/WebConfig.java
+++ src/main/java/study/brewer/config/WebConfig.java
@@ -1,9 +1,20 @@
 package study.brewer.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 import study.brewer.controller.CervejasController;
 
 @Configuration
@@ -17,5 +28,44 @@ import study.brewer.controller.CervejasController;
 public class WebConfig
 
     // Spring permite que utilizemos adaptadores que facilitam as nossas configurações
-    extends WebMvcConfigurerAdapter {
+    extends WebMvcConfigurerAdapter
+
+    // para poder receber o application context quando a aplicação subir
+        implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    // Com essa anotação, esse objeto fica disponível no contexto spring
+    @Bean
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
+    }
+
+    // Com essa anotação, esse objeto fica disponível no contexto spring
+    @Bean
+    public TemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setEnableSpringELCompiler(true);
+        engine.setTemplateResolver(templateResolver());
+        return engine;
+    }
+
+    private ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        // Thymeleaf precisa do applicationContext
+        resolver.setApplicationContext(applicationContext);
+        // os templates estarão nessa pasta, em resources
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        return resolver;
+    }
 }
--- src/main/java/study/brewer/controller/CervejasController.java
+++ src/main/java/study/brewer/controller/CervejasController.java
@@ -1,7 +1,12 @@
 package study.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CervejasController {
+    @RequestMapping("/cervejas/novo")
+    public String novo() {
+        return "cerveja/CadastroCerveja";
+    }
 }
