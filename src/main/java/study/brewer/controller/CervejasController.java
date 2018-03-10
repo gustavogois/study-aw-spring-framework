e191a121192fabf3b99a535de188a2ab7a170957
gustavogois <gustavo.gois@gmail.com>
2018-03-10 11:47:44 +0100

    Começando o cadastro de cerveja, recebendo uma requisição Post e imprimindo um valor na console

--- src/main/java/study/brewer/controller/CervejasController.java
+++ src/main/java/study/brewer/controller/CervejasController.java
@@ -2,11 +2,19 @@ package study.brewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import study.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
     @RequestMapping("/cervejas/novo")
     public String novo() {
+        return "cerveja/CadastroCerveja";
+    }
+
+    @RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+    public String cadastrar(Cerveja cerveja) {
+        System.out.println(">>> sku: " + cerveja.getSku());
         return "cerveja/CadastroCerveja";
     }
 }
