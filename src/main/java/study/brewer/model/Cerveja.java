bf5d6f8924432d67f57450bb277b288356553fc7
gustavogois <gustavo.gois@gmail.com>
2018-03-10 18:43:49 +0100

    Adicionando o campo descrição e mais Bean Validation

--- src/main/java/study/brewer/model/Cerveja.java
+++ src/main/java/study/brewer/model/Cerveja.java
@@ -2,12 +2,26 @@ package study.brewer.model;
 
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Size;
+
 public class Cerveja {
 
     @NotBlank
     private String sku;
 
+    @NotBlank
     private String nome;
+
+    @Size(min = 1, max = 50)
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
     public String getSku() {
         return sku;
